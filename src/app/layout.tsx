7cda19caf33f4824ccf2f9746569a10e7bed7a5e
Charles M <charlesensio@gmail.com>
2025-09-23 17:53:51 +0200

    tout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,20 +33,20 @@ export default function RootLayout({
         <div className="px-4">
             <div className="flex items-center justify-between">
                 <div className="flex shrink-0">
-                    <a aria-current="page" className="flex items-center" href="/">
+                    <Link aria-current="page" className="flex items-center" href="/">
                         <img className="h-7 w-auto"
                              src="Logo.png"
                              alt="93Moov"/>
                         <p className="sr-only">93Moove</p>
-                    </a>
+                    </Link>
                 </div>
                 <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
-                    <a className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                       href="#">Les prix</a>
+                    <Link className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                          href="/prix">Les prix</Link>
                 </div>
                 <div className="flex items-center justify-end gap-3">
                     <a className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-                       href="/login">S'inscrire</a>
+                       href="/login">S&#39;inscrire</a>
                     <a className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                        href="/login">Se connecter</a>
                 </div>
