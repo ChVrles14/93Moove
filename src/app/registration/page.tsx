453cb2a1c1868c30c72f882a56f72182a8f5848c
ymiled <miled.yousri.gf@gmail.com>
2025-09-30 17:02:42 +0200

    tien

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
                     <a href="#login"
                        className="font-medium text-blue-600 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150">
                         Ou connectez-vous
+
                     </a>
                 </p>
             </div>
