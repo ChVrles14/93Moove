f0067b95aa4b267007b51a5bfc643e03a49635ed
Charles M <charlesensio@gmail.com>
2025-09-30 17:17:41 +0200

    tout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
                     <a className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
                        href="/registration">S'inscrire</a>
                     <a className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                       href="/login">Se connecter</a>
+                       href="/connexion">Se connecter</a>
                 </div>
             </div>
         </div>
