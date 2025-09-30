6a180092c36277a69c58f4e68244fd785968d771
ymiled <miled.yousri.gf@gmail.com>
2025-09-30 17:56:39 +0200

    page création de compte

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                 </h2>
                 <p className="mt-2 text-center text-sm leading-5 max-w">
 
-                    <a href="#connexion"
+                    <a href="/connexion"
                        className="font-medium text-blue-600 hover:text-blue-500 focus:outline-none focus:underline transition ease-in-out duration-150">
                         Ou connectez-vous
 
@@ -53,12 +53,31 @@ export default function Home() {
                             </div>
                         </div>
 
+                        <div className="bg-white p-4 rounded-lg max-w-[350px]">
+                            <label className="text-gray-600 text-sm">
+                                Phone number
+                            </label>
+                            <div className="relative mt-2 max-w-xs text-gray-500">
+                                <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r pr-2">
+                                    <select className="text-sm outline-none rounded-lg h-full">
+                                        <option>FR</option>
+                                        <option>US</option>
+                                        <option>ES</option>
+                                        <option>RU</option>
+                                    </select>
+                                </div>
+                                <input type="number" placeholder="+33"
+                                       className="w-full pl-[4.5rem] pr-3 py-2 appearance-none bg-transparent outline-none border focus:border-slate-600 shadow-sm rounded-lg"/>
+                            </div>
+                        </div>
+
                         <div className="mt-6">
                             <label htmlFor="password" className="block text-sm font-medium leading-5 text-gray-700">
                                 Mot de passe
                             </label>
                             <div className="mt-1 rounded-md shadow-sm">
-                                <input id="password" name="password" placeholder="12 caractère min ( % & * + - / = ? ^ _ ' ] . )" type="password"
+                                <input id="password" name="password"
+                                       placeholder="12 caractère min ( % & * + - / = ? ^ _ ' ] . )" type="password"
                                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition text-gray-700 duration-150 ease-in-out sm:text-sm sm:leading-5"/>
                             </div>
                         </div>
@@ -69,7 +88,8 @@ export default function Home() {
                                 Confirmer mot de passe
                             </label>
                             <div className="mt-1 rounded-md shadow-sm">
-                                <input id="password_confirmation" name="password_confirmation" placeholder="12 ca. " type="password"
+                                <input id="password_confirmation" name="password_confirmation" placeholder="12 ca. "
+                                       type="password"
 
                                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition text-gray-700 duration-150 ease-in-out sm:text-sm sm:leading-5"/>
                             </div>
@@ -79,7 +99,7 @@ export default function Home() {
                     <span className="block w-full rounded-md shadow-sm">
                         <button type="submit"
                                 className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                            Créer un Compte
+                            <a href="/app">Créer un Compte</a>
                         </button>
                     </span>
                         </div>
