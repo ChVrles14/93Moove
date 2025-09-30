2a75a5d19a10dea8295c8864b829776ccff5ae29
dharbili <harbilidalil10@gmail.com>
2025-09-30 17:12:50 +0200

    zee

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
                 <form>
                     <div>
-                        <label className="block mb-2 text-indigo-500" htmlFor="username">Nom D'utilisateur</label>
+                        <label className="block mb-2 text-indigo-500" htmlFor="username">Nom d'utilisateur</label>
                         <input
                             className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
                             type="text" name="username" placeholder="écrivez votre nom d'utilisateur?"/>
