7a42e3f5fbf016aca94682baa359037e77cf644d
Charles M <charlesensio@gmail.com>
2025-10-07 15:04:25 +0200

    prisma

---
+++ prisma/migrations/20251007130239_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Animateurs" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nom" TEXT NOT NULL,
+    "prenom" TEXT NOT NULL,
+    "age" INTEGER NOT NULL
+);
