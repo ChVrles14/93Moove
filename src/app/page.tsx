67613dd84cdc64598bd0bf8b321e8e93336a4b4b
Charles M <charlesensio@gmail.com>
2025-10-14 15:02:12 +0200

    activité vedette depuis la base de donnée

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
                   <p>Bienvenue a toutes et a tous sur notre site qui vous accueille à bras ouverts et vous propose de
                       faire une activité physique régulière ou occasionel
                       vous pourrez vous inscrire sur notre site ou notre application à des cours et vous pourrez choisir
-                      parmi notre large catalogue d'activités.</p>
+                      parmi notre large catalogue d&#39;activités.</p>
 
               </div>
               < Image
@@ -29,100 +29,41 @@ export default async function Home() {
                   border-radius={500}
               />
               <section>
-                  <h2 className="text-2xl font-bold mb-6 border-l-4 border-white pl-3">Les activités vedettes</h2>
-                  <ul>
-                  {Activite.map((a) => (
+                  <h2 className="text-2xl font-bold mb-6 border-l-4 border-white pl-3">
+                      Les activités vedettes
+                  </h2>
 
-                      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-    <li key={a.id}>
-                      <div
-                          className="relative group bg-[#1c1c1c] rounded-4xl overflow-hidden shadow hover:shadow-lg transition">
-                          <img src="Handball.png" alt="Handball"
-                               className="w-full h-52 object-cover group-hover:scale-105 transition-transform duration-300"/>
-                          <div
-                              className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                              <button className="bg-orange-500 px-4 py-2 rounded-xl text-sm hover:bg-orange-700">S'inscrire
-                              </button>
-                          </div>
-                          <div className="p-2">
-                              <h3 className="text-sm font-semibold truncate">{a.nom}</h3>
-                          </div>
-                      </div>
-    </li>
-                      <div
-                          className="relative group bg-[#1c1c1c] rounded-4xl overflow-hidden shadow hover:shadow-lg transition">
-                          <img src="foot.png" alt="foot"
-                               className="w-full h-52 object-cover group-hover:scale-105 transition-transform duration-300"/>
-                          <div
-                              className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                              <button className="bg-orange-500 px-4 py-2 rounded-xl text-sm hover:bg-orange-700">S'inscrire
-                              </button>
-                          </div>
-                          <div className="p-2">
-                              <h3 className="text-sm font-semibold truncate">Football</h3>
-                          </div>
-                      </div>
-
-                      <div
-                          className="relative group bg-[#1c1c1c] rounded-4xl overflow-hidden shadow hover:shadow-lg transition">
-                          <img src="natation.png" alt="natation"
-                               className="w-full h-52 object-cover group-hover:scale-105 transition-transform duration-300"/>
-                          <div
-                              className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                              <button className="bg-orange-500 px-4 py-2 rounded-xl text-sm hover:bg-orange-700">S'inscrire
-                              </button>
-                          </div>
-                          <div className="p-2">
-                              <h3 className="text-sm font-semibold truncate">Natation</h3>
-                          </div>
-                      </div>
-
-                      <div
-                          className="relative group bg-[#1c1c1c] rounded-4xl overflow-hidden shadow hover:shadow-lg transition">
-                          <img src="basket.png" alt="basketball"
-                               className="w-full h-52 object-cover group-hover:scale-105 transition-transform duration-300"/>
-                          <div
-                              className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                              <button className="bg-orange-500 px-4 py-2 rounded-xl text-sm hover:bg-orange-700">S'inscrire
-                              </button>
-                          </div>
-                          <div className="p-2">
-                              <h3 className="text-sm font-semibold truncate">Basketball</h3>
-                          </div>
-                      </div>
-
-                      <div
-                          className="relative group bg-[#1c1c1c] rounded-4xl overflow-hidden shadow hover:shadow-lg transition">
-                          <img src="boxe.png" alt="Boxe"
-                               className="w-full h-52 object-cover group-hover:scale-105 transition-transform duration-300"/>
-                          <div
-                              className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                              <button className="bg-orange-500 px-4 py-2 rounded-xl text-sm hover:bg-orange-700">S'inscrire
-                              </button>
-                          </div>
-                          <div className="p-2">
-                              <h3 className="text-sm font-semibold truncate">Boxe</h3>
-                          </div>
-                      </div>
-
-                      <div
-                          className="relative group bg-[#1c1c1c] rounded-4xl overflow-hidden shadow hover:shadow-lg transition">
-                          <img src="judo.png" alt="judo"
-                               className="w-full h-52 object-cover group-hover:scale-105 transition-transform duration-300"/>
-                          <div
-                              className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
-                              <button className="bg-orange-500 px-4 py-2 rounded-xl text-sm hover:bg-orange-700">S'inscrire
-                              </button>
-                          </div>
-                          <div className="p-2">
-                              <h3 className="text-sm font-semibold truncate">judo</h3>
-                          </div>
-                      </div>
-
-                          )};
+                  {/* Grille horizontale pour toutes les cartes */}
+                  <ul className="flex gap-4 overflow-x-auto pb-2">
+                      {Activite.map((a) => (
+                          <li
+                              key={a.id}
+                              className="flex-none w-64 relative group bg-[#1c1c1c] rounded-4xl overflow-hidden shadow hover:shadow-lg transition"
+                          >
+                              <Image
+                                  src={`/${a.photo}`}
+                                  alt={a.nom}
+                                  width={400}
+                                  height={400}
+                                  className="w-full h-52 object-cover group-hover:scale-105 transition-transform duration-300"
+                              />
+                              <div className="absolute inset-0 bg-black bg-opacity-20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300">
+                                  <button className="bg-orange-500 px-4 py-2 rounded-xl text-sm hover:bg-orange-700">
+                                      S'inscrire
+                                  </button>
+                              </div>
+                              <div className="p-2">
+                                  <h3 className="text-sm font-semibold truncate">{a.nom}</h3>
+                              </div>
+                          </li>
+                      ))}
                   </ul>
-                    </section>
-                      </main>
+
+              </section>
+
+
+
+          </main>
                       </div>
 
 
