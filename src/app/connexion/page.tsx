7e3438d5c9c80c295902336d52c283964ef21cb5
Charles M <charlesensio@gmail.com>
2025-11-04 15:12:46 +0100

    update connexion

--- src/app/connexion/page.tsx
+++ src/app/connexion/page.tsx
@@ -2,53 +2,38 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex h-screen">
-            <div className="w-full max-w-xs m-auto bg-indigo-100 rounded p-5">
-                <header className="flex justify-center items-center h-20">
-                    < Image
-                        width={50}
-                        src="/Logo.png"
-                        height={50}
-                        className="flex items-center" alt={""}                    />
+        <div className="pt-50">
 
 
-                </header>
+            <form className="max-w-md mx-auto">
+                <div className="relative z-0 w-full mb-5 group">
+                    <input type="email" name="floating_email" id="floating_email"
+                           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                           placeholder=" " required/>
+                    <label htmlFor="floating_email"
+                           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                        Identifiant </label>
+                </div>
+                <div className="relative z-0 w-full mb-5 group">
+                    <input type="password" name="floating_password" id="floating_password"
+                           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                           placeholder=" " required/>
+                    <label htmlFor="floating_password"
+                           className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mot de passe</label>
+                </div>
 
-                <form>
-                    <div>
-                        <label className="block mb-2 text-indigo-500" htmlFor="username">Nom utilisation</label>
-                        <input
-                            className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-                            type="text" name="username" placeholder="écrivez votre nom d'utilisateur?"/>
-                    </div>
-                    <div>
-                        <label className="block mb-2 text-indigo-500" htmlFor="password">Mot de passe</label>
-                        <input
-                            className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-                            type="password" name="password" placeholder="écrivez votre mot de passe?"/>
-                    </div>
+<div className="flex items-center justify-between">
+    <button type="submit"
+                                                           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Connexion
+</button>
 
-                    <div>
-                        <a
-                            href="/registration"
-                            className="text-white bg-indigo-600 py-1.5 px-4 rounded w-full block text-center"
-                        >
-                            Se connecter
-                        </a>
-                    </div>
+    <button type="submit"
+            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Mot de passe oublié
+    </button>
+</div>
 
+            </form>
 
-                </form>
-
-
-                <footer>
-                    <a className="text-indigo-700 hover:text-pink-700 text-sm float-left" href="#">Mot de passe oublié?</a>
-
-                    <a     href="/registration"
-                           className="text-indigo-700 hover:text-pink-700 text-sm float-right" >Créer un compte
-                    </a>
-                </footer>
-            </div>
         </div>
     );
 }
